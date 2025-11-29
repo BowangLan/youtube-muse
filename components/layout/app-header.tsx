0b2ccc933efd968bf23f63fae0bcb5d954f00144
Jeffrey Lan <blan2@uw.edu>
2025-11-29 12:40:49 -0800

    updated header

--- components/layout/app-header.tsx
+++ components/layout/app-header.tsx
@@ -1,15 +1,15 @@
 export function AppHeader() {
   return (
     <div className="space-y-2 text-left motion-opacity-in-0 motion-blur-in-lg motion-delay-1200">
-      <p className="text-xs uppercase tracking-[0.4em] text-neutral-600">
-        YouTube Music Player
+      <p className="text-xs uppercase tracking-widest text-neutral-600">
+        Curate & Play & Focus
       </p>
       <div className="flex items-end justify-between text-neutral-200">
         {/* Site name */}
         <h1 className="md:text-3xl text-xl font-light leading-none">
           YouTube Muse
         </h1>
-        <span className="text-xs text-neutral-500">curate & play & focus</span>
+        {/* <span className="text-xs text-neutral-500">curate & play & focus</span> */}
       </div>
     </div>
   );
