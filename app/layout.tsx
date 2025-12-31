76da1dcc038ceefc1028f4222d51b434d1f2eb14
Jeffrey Lan <blan2@uw.edu>
2025-12-31 03:18:50 -0800

    added react-scan in dev mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -124,6 +124,12 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <StructuredData />
+        {process.env.NODE_ENV === "development" && (
+          <script
+            crossOrigin="anonymous"
+            src="//unpkg.com/react-scan/dist/auto.global.js"
+          />
+        )}
         {process.env.NODE_ENV === "production" && (
           <script
             defer
